abb8d82b19405c24a84ab634e8275b650846faf0
msauter <msauter@proqrent.de>
2020-06-29 18:00:49 +0200

    path bugfix

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname + '/dist/zb15062020'));
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/zb15062020/index.html'));
+app.use(express.static(__dirname + '/ZB15062020/dist/'));
+app.get('*', function(req, res) {
+  // res.sendFile(path.join(__dirname + '/dist/zb15062020/index.html'));
+  res.sendFile('./ZB15062020/dist/index.html');
 });
 app.listen(process.env.PORT || 8080);
